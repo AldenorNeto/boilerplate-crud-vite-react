52f2fde698ee2fc206073f18caf7f054bd36cfaf
Aldenor Neto 🦁 <aldenor.code@gmail.com>
2025-06-20 20:44:49 -0300

    refactor: remove unused routes for Gradista and Revisor components

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,11 @@
-import { Route, Routes } from "react-router-dom";
-import Comum from "./pages/comum";
-import Gradista from "./pages/gradista";
-import Revisor from "./pages/revisor";
+import { Route, Routes } from 'react-router-dom';
+import Comum from './pages/comum';
 
 function App() {
   return (
     <Routes>
       <Route path="/">
         <Route index element={<Comum />} />
-        <Route path="gradista">
-          <Route index element={<Gradista />} />
-        </Route>
-        <Route path="revisor">
-          <Route index element={<Revisor />} />
-        </Route>
       </Route>
     </Routes>
   );
